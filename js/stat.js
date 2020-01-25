384a8173d6db757250906aa39520ec0d84213e55
Aleksey-Shtr1h <52354908+Aleksey-Shtr1h@users.noreply.github.com>
2020-01-25 13:03:15 +0700

    Исправление ошибок ESlint

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -9,7 +10,7 @@ var GAP_Y = 20;
 var FONT_HEIGHT = 15;
 var GAP_BAR_Y = CLOUD_HEIGHT - CLOUD_Y;
 var BAR_WIDTH = 40;
-var BAR_HEIGHT =  150;
+var BAR_HEIGHT = 150;
 var FONT_STYLE = 'sans-serif 16px PT Mono';
 var TEXT_BASELINE = 'hanging';
 var TEXT_WIN = 'Ура вы победили!';
@@ -41,26 +42,22 @@ var getMaxElement = function (arr) {
 };
 
 var getRandomSaturation = function (min, max) {
-  return Math.round( Math.random() * (max - min) );
+  return Math.round(Math.random() * (max - min));
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud (ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)' );
-  renderCloud (ctx, CLOUD_X, CLOUD_Y, '#fff');
-
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   renderText(ctx, FONT_STYLE, TEXT_BASELINE, TEXT_WIN, CLOUD_X + GAP_X, CLOUD_Y + GAP);
   renderText(ctx, FONT_STYLE, TEXT_BASELINE, TEXT_RESULT, CLOUD_X + GAP_X, CLOUD_Y + GAP + GAP_Y);
 
-  var maxTime = getMaxElement (times);
+  var maxTime = getMaxElement(times);
 
-  // var colorBars = ['black', 'red', 'blue', 'green'];
-  // for (var i = 0, j = 0; i < players.length, j < colorBars.length; i++, j++)
   for (var i = 0; i < players.length; i++) {
-    // ctx.fillStyle = colorBars[j];
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'hsla(225, 100%,' + getRandomSaturation (MIN_PERCENT, MAX_PERCENT) + '%, 1)';
+      ctx.fillStyle = 'hsla(225, 100%,' + getRandomSaturation(MIN_PERCENT, MAX_PERCENT) + '%, 1)';
     }
     ctx.fillRect(CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i, GAP_BAR_Y, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
 
@@ -71,4 +68,3 @@ window.renderStatistics = function (ctx, players, times) {
     ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i, (CLOUD_Y + CLOUD_HEIGHT - GAP_Y - GAP_Y) - (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
-
